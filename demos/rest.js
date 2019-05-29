18e4565ffd27160fcdf15984d51b6f7e0865e20f
Rolando Santamaria Maso <kyberneees@gmail.com>
2019-05-29 16:58:46 +0200

    adding rest demo

---
+++ demos/rest.js
@@ -0,0 +1,16 @@
+const cero = require('../index')
+const { router, server } = cero()
+
+router.on('GET', '/', (req, res) => {
+  res.end()
+})
+
+router.on('GET', '/user/:id', (req, res, params) => {
+  res.end(params.id)
+})
+
+router.on('POST', '/user', (req, res) => {
+  res.end()
+})
+
+server.listen(3000)

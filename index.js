355d20752678e15c1db5ef781c2904269d9050b7
Nicholas Schamberg <schamberg.nicholas@gmail.com>
2021-03-22 15:40:25 +0300

    Linting

--- index.js
+++ index.js
@@ -7,24 +7,21 @@ module.exports = (config = {}) => {
   const server = config.server || http.createServer()
 
   config.prioRequestsProcessing = config.prioRequestsProcessing || server instanceof httpServer || server instanceof httpsServer
-  
-  /* 
+
+  /*
   Native server can also be https server, so we also need to check for it.
   Unfortunately, there appears to be no proper way to check for http2 server.
   */
-  
 
   if (config.prioRequestsProcessing) {
     server.on('request', (req, res) => {
       setImmediate(() => router.lookup(req, res))
     })
-  }
-  else {
+  } else {
     server.on('request', (req, res) => {
       router.lookup(req, res)
     })
   }
-  
 
   return {
     router,

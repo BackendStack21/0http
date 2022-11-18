2feb4c8bb334a038b5e91f80e209a96fda325e77
Rolando Santamaria Maso <kyberneees@gmail.com>
2022-11-18 20:10:45 +0100

    updating basic demo

--- demos/basic.js
+++ demos/basic.js
@@ -1,8 +1,14 @@
 const cero = require('./../index')
 const { router, server } = cero()
+router.use((req, res, next) => {
+  req.ctx = {
+    server: 'node'
+  }
 
+  return next()
+})
 router.get('/hi', (req, res) => {
-  res.end('Hello World!')
+  res.end(`Hello World from '${req.ctx.server}' engine!`)
 })
 
 server.listen(3000)

c1e937bbadd3797c1667d6ee37de6059ea7fe0d5
RyanWright <36499915+BronyDust@users.noreply.github.com>
2023-02-22 02:46:28 +0700

    Add 0http.d.ts file

---
+++ 0http.d.ts
@@ -0,0 +1,36 @@
+declare module "0http" {
+  interface IRouter {
+    lookup: (req: Request, res: Response, step?: VoidFunction) => void;
+  }
+
+  type SequentialRouter = IRouter &
+    import("trouter").default & {
+      on: (
+        method: import("trouter").Methods,
+        pattern: import("trouter").Pattern,
+        handlers: VoidFunction[]
+      ) => SequentialRouter;
+    };
+
+  interface IBuildServerAndRouterConfig<
+    R extends IRouter,
+    S extends import("http").Server
+  > {
+    router?: R;
+    server?: S;
+    prioRequestsProcessing?: boolean;
+  }
+
+  type BuildServerAndRouter = <
+    R extends IRouter = SequentialRouter,
+    S extends import("http").Server = import("http").Server
+  >(
+    config?: IBuildServerAndRouterConfig<R, S>
+  ) => {
+    router: R;
+    server: S;
+  };
+
+  const buildServerAndRouter: BuildServerAndRouter;
+  export default buildServerAndRouter;
+}

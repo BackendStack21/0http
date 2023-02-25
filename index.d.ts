8bbbb2bcd444a4a37a51a124b97463f1b617428c
Rolando Santamaria Maso <kyberneees@gmail.com>
2023-02-25 21:25:24 +0100

    update definition

--- index.d.ts
+++ index.d.ts
@@ -1,37 +1,59 @@
 import { type Server as HTTPServer, IncomingMessage, ServerResponse } from "http";
 import { type Server as HTTPSServer } from "https";
 import Trouter, { Methods, Pattern } from "trouter";
+import {
+  Http2SecureServer,
+  Http2ServerRequest,
+  Http2ServerResponse
+} from 'http2'
 
-type Server = HTTPServer | HTTPSServer;
+declare namespace zero {
+  export enum Protocol {
+    HTTP = 'http',
+    HTTPS = 'https',
+    HTTP2 = 'http2'
+  }
 
-declare namespace zeroHttp {
+  type Server<P extends Protocol> = P extends Protocol.HTTP2 ? Http2SecureServer : P extends Protocol.HTTPS ? HTTPSServer : HTTPServer
+  type StepFunction = (error?: unknown) => void
+
+  type Request<P extends Protocol> = P extends Protocol.HTTP2
+    ? Http2ServerRequest
+    : IncomingMessage
+
+  type Response<P extends Protocol> = P extends Protocol.HTTP2
+    ? Http2ServerResponse : ServerResponse
+
+  type RequestHandler<P extends Protocol> = (
+    req: Request<P>,
+    res: Response<P>,
+    next: (error?: unknown) => void
+  ) => void | Promise<unknown>
+
   interface IRouter {
-    lookup: (req: IncomingMessage, res: ServerResponse, step?: VoidFunction) => void;
+    lookup: (req: IncomingMessage, res: ServerResponse, step?: StepFunction) => void;
   }
 
-  class SequentialRouter extends Trouter<VoidFunction> implements IRouter {
-    id: string;
-    lookup(req: IncomingMessage, res: ServerResponse, step?: VoidFunction): void;
+  export class SequentialRouter<P extends Protocol> extends Trouter<RequestHandler<P>> implements IRouter {
+    id?: string;
+    lookup(req: IncomingMessage, res: ServerResponse, step?: StepFunction): void;
 
-    use(prefix: string, ...middlewares: VoidFunction[]): this;
-    use(...middlewares: VoidFunction[]): this;
+    use(prefix: string, ...middlewares: RequestHandler<P>[]): this;
+    use(...middlewares: RequestHandler<P>[]): this;
 
-    on(method: Methods, pattern: Pattern, ...middlewares: VoidFunction[]): this;
+    on(method: Methods, pattern: Pattern, ...middlewares: RequestHandler<P>[]): this;
   }
 
-  interface IBuildServerAndRouterConfig<R extends IRouter, S extends Server> {
+  interface IBuildServerAndRouterConfig<Server, R extends IRouter> {
     router?: R;
-    server?: S;
+    server?: Server;
     prioRequestsProcessing?: boolean;
   }
+
+  export function zero<P extends Protocol, R extends IRouter>(config?: IBuildServerAndRouterConfig<Server<P>, R>): {
+    server: Server<P>,
+    router: R
+  };
 }
 
-declare function buildServerAndRouter<
-  R extends zeroHttp.IRouter = zeroHttp.SequentialRouter,
-  S extends Server = HTTPServer
->(config?: zeroHttp.IBuildServerAndRouterConfig<R, S>): {
-  server: S,
-  router: R
-};
-
-export = buildServerAndRouter;
+export default zero;

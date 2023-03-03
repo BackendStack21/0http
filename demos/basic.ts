fd03d5196ef4cf50ecac9ada85ee9a1e6fccd749
Rolando Santamaria Maso <kyberneees@gmail.com>
2023-03-03 20:09:05 +0100

    updating typescript example

--- demos/basic.ts
+++ demos/basic.ts
@@ -1,9 +1,11 @@
-import zero from './../index'
-import sequentialBuilder from './../lib/router/sequential'
-import { Protocol } from './../common'
+import zero from '../index'
+import { createServer } from 'node:http'
+import createRouter from '../lib/router/sequential'
+import { Protocol } from '../common'
 
 const { router, server } = zero<Protocol.HTTP>({
-  router: sequentialBuilder<Protocol.HTTP>()
+  router: createRouter<Protocol.HTTP>(),
+  server: createServer()
 })
 
 router.use((req, res, next) => {
